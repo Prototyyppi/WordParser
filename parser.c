3e1b53b0308fffbba6864c0ff4c28bbe701d6818
Mac <jani.laitinen@gmx.com>
2018-04-09 17:49:35 +0300

    Work continues, next cleanup needed, maybe scandi characters too

--- parser.c
+++ parser.c
@@ -8,6 +8,7 @@
 #include "parser.h"
 
 struct word* hashArray[SIZE];
+struct word* secondArray[SIZE];
 static int asd = 0;
 FILE* document;
 
@@ -22,12 +23,14 @@ int main(int argc, char** argv) {
 	while (((sana = get_word()) )!= NULL) {
 	if (sana == NULL) return 0;
 	//1. get word
-	int key = hashCode(sana);
+	int key = hash(sana);
 	//2. check is it listed if it is continue, else add
 	ret = find(key, sana);
 	//3. save
 	//if (asd==100) return 0;
 	}
+	int max_ind = strip_nulls();
+	quick_sort(0, max_ind);
 	print_result();
 	return EXIT_SUCCESS;
 }
@@ -36,7 +39,7 @@ void insert(char* sana) {
 
 	struct word *item = malloc(sizeof(struct word));
 	strcpy(sana, item->word);
-	item->key = hashCode(sana);
+	item->key = hash(sana);
 
 	//get the hash 
 	int hashIndex = 0;
@@ -63,12 +66,15 @@ int find(int key, char* sana) {
 	//printf("%s", hashArray[index]->word);
 	if (!(strcmp(hashArray[index]->word, sana))) {
 		hashArray[index]->occurences++;
-		printf("Found new %s (%d)\n",hashArray[index]->word, hashArray[index]->occurences);
+		//printf("Found new %s (%d)\n",hashArray[index]->word, hashArray[index]->occurences);
 		asd++;
 		return NOK;
 	}
 	//go to next cell
 	++index;
+	//wrap around as needed
+	index %= SIZE;
+	//printf("index %d",index);
 	}
 	//printf("New %s\n", sana);
 	asd++;
@@ -84,13 +90,13 @@ int add(int index, int key, char* sana) {
 	strcpy(item->word, sana);
 	item->occurences = 1;
 	while (hashArray[index] != NULL)
-		index++;
+		index++;// %= SIZE;
 	hashArray[index] = item;
 	//printf("%d index", index);
 	return OK;
 }
 
-int hashCode(char* sana) {
+int hash(char* sana) {
 	int hash = 0;
 	hash = sana[0] << 24 | sana[1] << 16 | sana[2] << 8 | sana[3];
 	return hash % SIZE;
@@ -130,9 +136,80 @@ int init_file(int argc, char** argv) {
 }
 
 int print_result() {
-	for (int index = 0; index<SIZE; index++){
+	for (int index = 0; index<MAX_PRINT_SIZE; index++){
 		if(hashArray[index] != NULL)
-		printf("%s %d\n", hashArray[index]->word, hashArray[index]->occurences);
+		printf("%d %s %d\n", index, hashArray[index]->word, hashArray[index]->occurences);
 	}
 	return OK;
+}
+
+int strip_nulls() {
+	int scan = 0;
+	int prev = 0;
+	int index;
+	for (index = 0; index<SIZE; index++) {
+		if (hashArray[scan] == NULL){
+			while (hashArray[scan] == NULL)
+				scan++;
+		
+		}
+		if (scan >= SIZE)
+			break;
+		if (index != scan)
+			hashArray[index] = hashArray[scan];
+		scan++;
+		//printf("(%d scan)", scan);
+		//printf("(%d VALUE)", index);
+	}
+	int sec_index = index;
+	for (; sec_index<SIZE; sec_index++) {
+		hashArray[sec_index] = NULL;
+	}
+	//printf("%d VALUE", index);
+	return index-1;
+}
+
+int quick_sort(int low_index, int high_index) {
+	if (low_index < high_index) {
+
+		int middle_index = partition(low_index, high_index);
+		
+		quick_sort(low_index, middle_index - 1);  // Before pi
+
+		quick_sort(middle_index + 1, high_index); // After pi
+
+	}
+	return OK;
+}
+
+
+int partition(int low_index, int high_index)
+{
+
+	// pivot (Element to be placed at right position)
+	struct word* pivot = hashArray[high_index];
+	struct word* temp;
+
+	int i = low_index-1;
+	//while (hashArray[low_index] == NULL) ++low_index;
+
+	for (int j = low_index; j <= high_index - 1; j++)
+	{
+
+		if (hashArray[j]->occurences >= pivot->occurences) {
+			++i;
+			if (i != j){
+			temp = hashArray[i];
+			hashArray[i] = hashArray[j];
+			hashArray[j] = temp;
+			}
+			
+		}
+	}
+
+	//while (hashArray[i+1] == NULL) ++i;
+	temp = hashArray[i+1];
+	hashArray[i+1] = hashArray[high_index];
+	hashArray[high_index] = temp;
+	return i+1;
 }
--- parser.h
+++ parser.h
@@ -1,7 +1,12 @@
+/*
+ * Header for parser
+ *
+ */
 
+//Size should be over wanted value to make indexes unique and to fit the wanted words
 #define SIZE 1000
 #define WORD_MAX_LEN 100
-
+#define MAX_PRINT_SIZE 100
 struct word {
 	char word[WORD_MAX_LEN];
 	int occurences;
@@ -13,16 +18,30 @@ enum err {
 	NOK
 };
 
+//inline may increase speed
+static inline const char* err2str(enum err err)
+{
+	static const char* strings[] = { "OK", "NOK"};
+
+	return strings[err];
+}
+
 void insert(char* sana);
 
 int find(int key, char sana[]);
 
 int add(int index, int key, char* sana);
 
-int hashCode(char* sana);
+int hash(char* sana);
 
 int init_file(int, char**);
 
 char* get_word();
 
 int print_result();
+
+int strip_nulls();
+
+int quick_sort(int low_index, int high_index);
+
+int partition(int low_index, int high_index);
